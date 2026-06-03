a09f331d4200049e72e1f9b5c97f2b83bffd8790
tungyao <1922527051@qq.com>
2026-06-03 09:13:52 +0800

    fixproxy

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -194,7 +194,7 @@ elseif(EXISTS "${NLOHMANN_JSON_LOCAL_SOURCE}/CMakeLists.txt")
 else()
     FetchContent_Declare(
         nlohmann_json
-        GIT_REPOSITORY https://github.com/nlohmann/json.git
+        GIT_REPOSITORY https://gh-proxy.org/https://github.com/nlohmann/json.git
         GIT_TAG v3.11.3
     )
     FetchContent_MakeAvailable(nlohmann_json)
@@ -213,7 +213,7 @@ if(EXISTS "${SPDLOG_LOCAL_SOURCE}/CMakeLists.txt")
 else()
     FetchContent_Declare(
         spdlog
-        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_REPOSITORY https://gh-proxy.org/https://github.com/gabime/spdlog.git
         GIT_TAG v1.14.1
     )
     FetchContent_MakeAvailable(spdlog)
